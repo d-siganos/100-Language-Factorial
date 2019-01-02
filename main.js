e35107db6cbf5b31fa3f07f797b79c66028cef34
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-01-02 16:35:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+var number, result;
 
+function factorial() {
+  if (number <= 1) {
+		return 1;
+	} else {
+		return factorial(number-1)*number;
+	}
+  return result;
+}
+
+number = prompt("Enter a number", "");
+result = factorial();
+alert(result)
