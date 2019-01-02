910e0f51ce46e7b1bed5e0a4e2fccfeff4f8a0cf
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-01-02 16:53:20 +0200

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,17 @@
+var number, result;
+
+dynamic factorial() {
+  if (number <= 1) {
+    return 1;
+  }
+  if (number > 1) {
+    result = number * factorial(number-1);
+    return result;
+  }
+}
+
+
+main() {
+  number = 1;
+  result = factorial();
+}
