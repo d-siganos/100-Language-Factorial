c9c3c783cb2c0ce4e5050909731f3a6af2aaee71
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-01-03 10:24:32 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,23 @@
+package factorialjava;
+
+import java.util.Scanner;
+
+public class FizzBuzzJava {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        String number = scan.nextLine();
+        int x = Integer.parseInt(number);
+        int result = factorial(x);
+        System.out.println(result);
+    }
+    
+    public static void factorial(int number) {
+        if(number <= 1) {
+            return 1;
+        } else {
+            return number*factorial(number-1);
+        }
+    }
+}
