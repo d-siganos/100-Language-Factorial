92447f0585883984bbc8e74be81ef0cd049056d9
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-01-03 12:56:23 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int number;
+    int result;
+    std::cout << "Enter a number: \n";
+    cin >> i;
+    result = factorial(number);
+    cout << result;
+    return 0
+}
+
