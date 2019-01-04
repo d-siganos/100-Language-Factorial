ff894dbc6b458dfe1ac4a7fcdf0fba2a167743cf
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-01-04 10:48:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+
 int main()
 {
    int number;
    printf("Enter a number: ");
    fgets(number,20,stdin);
    result = factorial(number)
+   printf(result);
    return 0;
 }
 
 int max(int number) {
-   int result;
    if (number <= 1)
       return 1;
    else
