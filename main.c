d623fab74be84a0641f134ed1d2d21df295a1ee6
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-01-04 10:46:42 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,3 +7,14 @@ int main()
    result = factorial()
    return 0;
 }
+
+int max(int number) {
+   int result;
+ 
+   if (num1 > num2)
+      result = num1;
+   else
+      result = num2;
+ 
+   return result; 
+}
