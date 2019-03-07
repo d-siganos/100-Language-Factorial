f6080cef8a770251b68cc009c6186df053e53186
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-03-07 07:52:32 +0200

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,8 +1,6 @@
 using System;  
-  public class FactorialExample  
-   {  
-     public static void Main(string[] args)  
-      {  
+  public class FactorialExample {  
+     public static void Main(string[] args) {  
        int i,fact=1,number;      
        Console.Write("Enter a number: ");      
        number= int.Parse(Console.ReadLine());     
