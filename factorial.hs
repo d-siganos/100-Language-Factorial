393ca125f04e7464afce5aaa9afe1f99a9ae9f2a
Dandigit <37099129+Dandigit@users.noreply.github.com>
2019-03-09 06:22:54 +1000

    Update factorial.hs

--- factorial.hs
+++ factorial.hs
@@ -1,5 +1,17 @@
-module Factorial where
+module Main where
 
-factorial :: Integer -> Integer
+import System.IO
+
+main = do
+    putStr "Factorial: Enter an integer > "
+    hFlush stdout
+    input <- getLine
+
+    let number = read input :: Integer
+    let result = factorial $ number
+
+    print result
+
+factorial :: (Integral a) => a -> a
 factorial 0 = 1
-factorial n = n * factorial (n - 1)
+factorial n = n * (factorial $ n - 1)
