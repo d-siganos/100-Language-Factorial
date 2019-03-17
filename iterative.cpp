d713700683201c42bde83d7f7d514534a105b1e0
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-03-17 10:33:31 +0200

    Update iterative.cpp

--- iterative.cpp
+++ iterative.cpp
@@ -8,17 +8,16 @@ int main()
 {
     int number, result;
     cout << "Enter a number: \n";
-    cin >> i;
+    cin >> number;
     result = factorial(number);
     cout << result;
     return 0;
 }
 
-int factorial(int number)
-{
+int factorial(int number) {
     int res = 1;
-    for (int i = 0; i < number; i++) {
-        res *= i;
-    }
+    for(int i = 1; i <= number; i++) {
+    	res = res * i; 
+	}
     return res;
 }
