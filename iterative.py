88f248a0f9df8f230884d6ebf71e7c9526dd4275
-Bonfire- <40984528+BonfireScratch@users.noreply.github.com>
2019-06-06 21:17:46 +0300

    Create iterative.py

---
+++ iterative.py
@@ -0,0 +1,9 @@
+number = input("Enter an integer: ")
+result = factorial(number)
+print(result)
+
+def result(number):
+  result = 1
+  for i in range(1, number):
+    result = result * i
+  return result
